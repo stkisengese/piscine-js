ef3cb2a3a101db35034784a33dadc814dfd37ffc
skisenge <skisengese@gmail.com>
2024-08-27 01:27:54 +0300

    feat: initial commit

---
+++ returns.js
@@ -0,0 +1,14 @@
+// Instructions
+
+// Create the following functions:
+
+//     id that takes one argument and returns it.
+//     getLength that takes an array or a string and returns its length.
+
+function id(value) {
+    return value;
+}
+
+function getLength(input) {
+    return input.length;
+}
