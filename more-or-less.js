eaa6a18870582e92512f9a336c380686f112775b
skisenge <skisengese@gmail.com>
2024-08-27 01:17:31 +0300

    feat: initial commit

---
+++ more-or-less.js
@@ -0,0 +1,19 @@
+// Create 4 functions:
+
+//     more that takes 1 argument and adds 1 to it.
+//     less that takes 1 argument and subtracts 1 from it.
+//     add that takes 2 arguments and adds them together.
+//     sub that takes 2 arguments and subtracts the second argument from the first.
+
+// All the functions must return the result of the operations specified above.
+
+const more = (n) => n+1;
+const less = (n) => n-1;
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+
+// console.log(more(4));
+// console.log(less(5));
+// console.log(add(3, 4));
+// console.log(sub(7, 2));
