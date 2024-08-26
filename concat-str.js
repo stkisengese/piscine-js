c4a42b31eb843807988a3b093ccf7a080e801465
skisenge <skisengese@gmail.com>
2024-08-27 01:51:28 +0300

    fix: Spacing again

--- concat-str.js
+++ concat-str.js
@@ -2,6 +2,6 @@
 
 // Create a function named concatStr which takes 2 arguments and concatenates them.
 
-function concatStr(a, b) {
-    return a + " " + b;
+function concatStr(str1, str2) {
+    return str1 + str2;
 }
