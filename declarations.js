e7507dda2a171785ff3527adb1858ff2d64a2750
skisenge <skisengese@gmail.com>
2024-08-26 21:43:13 +0300

    fix: object edit

--- declarations.js
+++ declarations.js
@@ -1,16 +1,16 @@
-const escapeStr = "`\\/\"'"
-const arr = Object.freeze([4, '2'])
+const escapeStr = "`\\/\"'";
+const arr = Object.freeze([4, '2']);
 const obj = Object.freeze({
     str : "Hello",
     num : 42,
     bool : false,
     undef : undefined,
-})
+});
 const nested = Object.freeze({
     arr : [4, undefined, '2'],
     obj : {
         str : "Hello",
         num : 42,
         bool : false,
-    }
-})
+    },
+});
