81a1ed64a2aef7902a9ce7710de76a5d1a6df43b
skisenge <skisengese@gmail.com>
2024-08-27 02:47:23 +0300

    feat: initial commit

---
+++ dog-years.js
@@ -0,0 +1,20 @@
+const orbitalPeriods = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132  
+
+  };
+
+  function dogYears(planet, ageInSeconds) {
+    const earthYears = ageInSeconds / 31557600;
+    const planetYears = earthYears / orbitalPeriods[planet];
+    const dogYears = planetYears * 7;
+    return dogYears.toFixed(2);
+  }
+
+  //console.log(dogYears('earth', 1000000000)); 
