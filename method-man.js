2b8f816a3c424c5533318b0eda3b6dc819da1b42
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-08-27 10:54:19 +0000

    feat: initial commit

---
+++ method-man.js
@@ -0,0 +1,26 @@
+// split string by spaces
+function words(str) {
+    return str.split(' ');
+}
+
+// join array of strings by spaces
+function sentence(arr) {
+    return arr.join(' ');
+}
+
+// write string to upper case
+function yell(str) {
+    return str.toUpperCase();
+}
+// write string to lower case sorrounded by '*'
+function whisper(str) {
+    return `*${str.toLowerCase()}*`;
+    // return "*" + str.toLowerCase + "*";
+}
+
+// upper case first letter and lowercase the rest in a string
+function capitalize(str) {
+    if (str.lenght === 0) return str;
+    return str[0].toUpperCase + str.slice(1).toLowerCase;
+    // return str.charAt(0).toUpperCase + str.substr(1).toLowerCase;
+}
