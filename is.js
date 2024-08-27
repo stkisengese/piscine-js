d804f1562bac8d44430256806fc52221983d36d2
skisenge <skisengese@gmail.com>
2024-08-27 20:43:26 +0300

    fix: check Array.isArray()

--- is.js
+++ is.js
@@ -1,9 +1,9 @@
 // const is = {};
 
 // Add fuction properties to is object to check value type
-is.num = (value) => typeof value === 'number' && !isNaN(value);
+is.num = (value) => typeof value === 'number' && !isNaN(value) && !Array.isArray(value);
 is.nan = (value) => typeof value === 'number' && isNaN(value);
-is.str = (value) => typeof value === 'string';
+is.str = (value) => typeof value === 'string' && !Array.isArray;
 is.bool = (value) => typeof value === 'boolean';
 is.obj = (value) => typeof value === 'object' && value !== null && !Array.isArray(value);
 is.fun = (value) => typeof value === 'function';
@@ -14,15 +14,15 @@ is.truthy = (value) => !!value;
 is.falsy = (value) => !value;
 
 
-console.log(is.num(5)); // true
-console.log(is.num('ciao')); // false
-console.log(is.nan(NaN)); // true
-console.log(is.str('hello')); // true
-console.log(is.bool(true)); // true
-console.log(is.undef(undefined)); // true
-console.log(is.def(null)); // true
-console.log(is.arr([1, 2, 3])); // true
-console.log(is.obj({ a: 1 })); // true
-console.log(is.fun(function() {})); // true
-console.log(is.truthy(1)); // true
-console.log(is.falsy(0)); // true
+// console.log(is.num(5)); // true
+// console.log(is.num('ciao')); // false
+// console.log(is.nan(NaN)); // true
+// console.log(is.str('hello')); // true
+// console.log(is.bool(true)); // true
+// console.log(is.undef(undefined)); // true
+// console.log(is.def(null)); // true
+// console.log(is.arr([1, 2, 3])); // true
+// console.log(is.obj({ a: 1 })); // true
+// console.log(is.fun(function() {})); // true
+// console.log(is.truthy(1)); // true
+// console.log(is.falsy(0)); // true
