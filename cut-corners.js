8e25fdbc3a76bd2c786fdd9c9af604a34b7ccccb
skisenge <skisengese@gmail.com>
2024-08-29 00:43:37 +0300

    feat: removed usage

--- cut-corners.js
+++ cut-corners.js
@@ -30,9 +30,9 @@ function trunc(num) {
   return intPart;
 }
 
-// Usage
-const nums = [3.7, -3.7, 3.1, -3.1];
-console.log(nums.map(round)); // [ 4, -4, 3, -3 ]
-console.log(nums.map(floor)); // [ 3, -4, 3, -4 ]
-console.log(nums.map(trunc)); // [ 3, -3, 3, -3 ]
-console.log(nums.map(ceil)); // [ 4, -3, 4, -3 ]
+// // Usage
+// const nums = [3.7, -3.7, 3.1, -3.1];
+// console.log(nums.map(round)); // [ 4, -4, 3, -3 ]
+// console.log(nums.map(floor)); // [ 3, -4, 3, -4 ]
+// console.log(nums.map(trunc)); // [ 3, -3, 3, -3 ]
+// console.log(nums.map(ceil)); // [ 4, -3, 4, -3 ]
