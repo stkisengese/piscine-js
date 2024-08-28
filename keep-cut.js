1fbb57e5f5da30daf60f035582ab78e500e2253d
skisenge <skisengese@gmail.com>
2024-08-28 23:15:28 +0300

    fix: out of range error

--- keep-cut.js
+++ keep-cut.js
@@ -1,16 +1,17 @@
-const cutFirst = (str) => slice(str, 2);
-const cutLast = (str) => slice(str, 0, -2);
-const cutFirstLast = (str) => slice(str, 2, -2);
-const keepFirst = (str) => slice(str, 0, 2);
-const keepLast = (str) => slice(str, -2);
-const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+const cutFirst = (str) => str.length > 2 ? slice(str, 2) : str;
+const cutLast = (str) => str.length > 2 ? slice(str, 0, -2): str;
+const cutFirstLast = (str) => str.length > 4 ? slice(str, 2, -2) : str;
+const keepFirst = (str) => str.length > 2 ? slice(str, 0, 2) : str;
+const keepLast = (str) => str.length > 2 ? slice(str, -2) : str;
+const keepFirstLast = (str) => str.length > 4 ? keepFirst(str) + keepLast(str) : str;
 
-console.log(cutFirst("hello")) // "llo"
-console.log(cutLast("hello")); // Output: hell
+
+console.log(cutFirst("l")) // "llo"
+console.log(cutLast("lo")); // Output: hell
 console.log(cutFirstLast("heworldlo")); // Output: world
-console.log(keepFirstLast("23Hegfafdllo23")); // Output: Helo
+console.log(keepFirstLast("233")); // Output: Helo
 console.log(keepFirst("Hello")); // Output: H
-console.log(keepLast("Hello")); // Output: lo
+console.log(keepLast("H")); // Output: lo
 
 
 
