757e4dd90bcb585854fac4f8a4296b08e6b1a941
skisenge <skisengese@gmail.com>
2024-08-30 01:47:39 +0300

    fix: check for invalid strings 3

--- find-expression.js
+++ find-expression.js
@@ -1,15 +1,32 @@
-function findExpression(target) {
-  function helper(current, expression) {
-    if (current === target) {
-      return expression;
-    }
-    if (current > target || /[5-9]/.test(expression)) {
-      return undefined;
-    }
-    return (
-      helper(current + 4, expression + "+4") ||
-      helper(current * 2, expression + "*2")
-    );
+// const add4 = '+4'
+// const mul2 = '*2'
+
+function findExpression(number, current = 1, expression = '1') {
+  // Base cases
+  if (current === number) {
+    return expression;
   }
-  return helper(1, "1");
+  if (current > number) {
+    return undefined;
+  }
+  const addResult = findExpression(number, current + 4, `${expression} ${add4}`);
+  if (addResult) {
+    return addResult;
+  }
+
+  const mulResult = findExpression(number, current * 2, `${expression} ${mul2}`);
+  if (mulResult) {
+    return mulResult;
+  }
+
+  return undefined;
 }
+
+// // Test cases
+// console.log(findExpression(8));  // Expected: 1 *2 *2 +4
+// console.log(findExpression(14)); // Expected: 1 *2 +4 *2
+// console.log(findExpression(17)); // Expected: 1 +4 *2 +4 +4
+// console.log(findExpression(2));  // Expected: 1 *2
+// console.log(findExpression(4));  // Expected: 1 +4
+// console.log(findExpression(1));  // Expected: 1
+// console.log(findExpression(23)); // Expected: undefined
