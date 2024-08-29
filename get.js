d9daa58e7c745f2a07e1aab1575c4466f220a7b9
skisenge <skisengese@gmail.com>
2024-08-29 06:11:56 +0300

    feat: undefined error for non keys

--- get.js
+++ get.js
@@ -1,9 +1,12 @@
 function get(src, path) {
   const keys = path.split(".");
   let result = src;
-  keys.forEach((key) => {
-    result = result[key] !== undefined ? result[key] : typeof result[key];
-  });
+  for (const key of keys) {
+    if (result === undefined || !result.hasOwnProperty(key)) {
+      return "undefined";
+    }
+    result = result[key];
+  }
   return result;
 }
 
