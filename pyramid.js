468a6a6b2cf1c642fa75e42552ab9349da1e8aee
skisenge <skisengese@gmail.com>
2024-08-29 21:37:18 +0300

    feat: initial commit

---
+++ pyramid.js
@@ -0,0 +1,17 @@
+function pyramid(char, height) {
+  let result = "";
+  let i = 1;
+  let j = 1;
+  while (i <= height) {
+    result += " ".repeat(height - i) + char.repeat(j);
+    i++;
+    j += 2;
+    if (i <= height) {
+      result += "\n";
+    }
+  }
+  return result;
+}
+
+console.log(pyramid("#", 5));
+console.log(pyramid("$", 7));
