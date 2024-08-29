9477a62f123feea5e06173704403b26bd9968b5c
skisenge <skisengese@gmail.com>
2024-08-30 01:44:39 +0300

    fix: check for invalid strings

--- find-expression.js
+++ find-expression.js
@@ -1,6 +1,3 @@
-// const add4 = '+4';
-// const mul2 = '*2';
-
 function findExpression(target) {
   function helper(current, expression) {
     if (current === target) {
@@ -10,17 +7,9 @@ function findExpression(target) {
       return undefined;
     }
     return (
-      helper(current + 4, expression + '+4') ||
-      helper(current * 2, expression + '*2')
+      helper(current + 4, expression + "+4") ||
+      helper(current * 2, expression + "*2")
     );
   }
   return helper(1, "1");
 }
-
-console.log(findExpression(8)); // Expected: 1 *2 *2 +4
-console.log(findExpression(14)); // Expected: 1 *2 +4 *2
-console.log(findExpression(17)); // Expected: 1 +4 *2 +4 +4
-console.log(findExpression(8)); // Expected: 1 *2
-console.log(findExpression(4)); // Expected: 1 +4
-console.log(findExpression(1)); // Expected: 1
-console.log(findExpression(23)); // Expected: undefined
