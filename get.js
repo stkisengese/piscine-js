b3992af9dba04982d9628358ca18192a9ea6ae7f
skisenge <skisengese@gmail.com>
2024-08-29 06:05:08 +0300

    feat: undefined key search

--- get.js
+++ get.js
@@ -2,11 +2,11 @@ function get(src, path) {
   const keys = path.split(".");
   let result = src;
   keys.forEach((key) => {
-    result = result[key];
+    result = result[key] !== undefined ? result[key] : typeof result[key];
   });
   return result;
 }
 
 const src = { nested: { key: "peekaboo" } };
-const path = "nested.key";
+const path = "nested.ke";
 console.log(get(src, path)); // -> 'peekaboo'
