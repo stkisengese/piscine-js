b42f2b2451de364bb0bebf7c920796883dfa3500
skisenge <skisengese@gmail.com>
2024-08-30 16:51:56 +0300

    fix: count null

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
 function sameAmount(inputStr, regex1, regex2) {
-  const count1 = inputStr.match(regex1) ?? [];
-  const count2 = inputStr.match(regex2) ?? [];
+  const count1 = inputStr.match(regex1) || [];
+  const count2 = inputStr.match(regex2) || [];
 
-  return count1 === count2;
+  return count1.length === count2.length;
 }
