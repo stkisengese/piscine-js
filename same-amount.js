5c2da194c8cb437df960ae9f03a5ff46481c8630
skisenge <skisengese@gmail.com>
2024-08-30 16:47:58 +0300

    fix: count match

--- same-amount.js
+++ same-amount.js
@@ -2,5 +2,5 @@ function sameAmount(inputStr, regex1, regex2) {
   const count1 = inputStr.match(regex1) ?? [];
   const count2 = inputStr.match(regex2) ?? [];
 
-  return count1.length === count2.lenght;
+  return count1 === count2;
 }
