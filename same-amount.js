f219b4c6334fb5305cf02d12de0bd0fd75873ca7
skisenge <skisengese@gmail.com>
2024-08-30 16:37:18 +0300

    fix: null output

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,6 @@
 function sameAmount(inputStr, regex1, regex2) {
-    const match1 = inputStr.match(regex1);
-    const match2 = inputStr.match(regex2);
-    return match1.length === match2.length
+  const count1 = inputStr.match(regex1) ?? [];
+  const count2 = inputStr.match(regex2) ?? [];
+
+  return count1.length === count2.lenght;
 }
