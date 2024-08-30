f427f9ed8b5829cf93489739c9249bb8cb75766a
skisenge <skisengese@gmail.com>
2024-08-30 16:58:24 +0300

    fix: failling count

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
 function sameAmount(inputStr, regex1, regex2) {
-  const count1 = inputStr.match(regex1) || [];
-  const count2 = inputStr.match(regex2) || [];
+  const count1 = (inputStr.match(regex1) || []).lenght;
+  const count2 = (inputStr.match(regex2) || []).length;
 
-  return count1.length === count2.length;
+  return count1 === count2;
 }
