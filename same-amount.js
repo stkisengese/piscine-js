791fbd741226fbcb7b5ab4b20a80bfd2e4fb296d
skisenge <skisengese@gmail.com>
2024-08-30 16:59:49 +0300

    feat: use global flag

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,9 @@
 function sameAmount(inputStr, regex1, regex2) {
-  const count1 = (inputStr.match(regex1) || []).lenght;
-  const count2 = (inputStr.match(regex2) || []).length;
+  const r1 = new RegExp(regex1, "g");
+  const r2 = new RegExp(regex2, "g");
+
+  const count1 = (inputStr.match(r1) || []).length;
+  const count2 = (inputStr.match(r2) || []).length;
 
   return count1 === count2;
 }
