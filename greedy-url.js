cdc56a5fc6d0fe55f25e017945bc5d72d36cdcd9
skisenge <skisengese@gmail.com>
2024-08-30 18:31:52 +0300

    fix: greedy querry 3

--- greedy-url.js
+++ greedy-url.js
@@ -7,7 +7,9 @@ function getURL(dataSet) {
 
 // greedyQuery//returns URLs from the dataSet, with at least 3 query parameters.
 function greedyQuery(dataSet) {
-  const greedyPattern = /(https?:\/\/[\w.]+\/?[\w-.~:/?#[\]@!$&'()*+,;=]+)\?(.+?)(?=&|=|$)/g;
+  const greedyPattern =
+    /(https?:\/\/[\w.]+\/?[\w-.~:/?#[\]@!$&'()*+,;=]+)\?(.{3,})(?=&|=|$)/g;
+  //   const greedyPattern = /(https?:\/\/[\w.]+\/?[\w-.~:/?#[\]@!$&'()*+,;=]+)\?(.+?)(?=&|=|$)/g;
   return dataSet.match(greedyPattern) || [];
 }
 
