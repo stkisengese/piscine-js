45dfa27e4de4c1256e00814f6c90bc65a31911b5
skisenge <skisengese@gmail.com>
2024-08-30 04:32:54 +0300

    fix: fix zero sum

--- sums.js
+++ sums.js
@@ -1,19 +1,19 @@
 function sums(n) {
-    let result = [];
-    function backtrack(start, remaining, path) {
-        if (remaining === 0) {
-            result.push([...path]);
-            return;
-        }
-        for (let i = start; i <= remaining; i++) {
-            path.push(i);
-            backtrack(i, remaining - i, path);
-            path.pop();
-        }
-    }   
-    backtrack(1, n, []);
-    return result;
+  let result = [];
+  if (n === 0) return result;
+  function backtrack(start, remaining, path) {
+    if (remaining === 0) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = start; i <= remaining; i++) {
+      path.push(i);
+      backtrack(i, remaining - i, path);
+      path.pop();
+    }
+  }
+  backtrack(1, n, []);
+  return result;
 }
 
-console.log(sums(6)); // [ [1, 1, 1, 1], [1, 1, 2], [1, 3], [2, 2] ]
-
+console.log(sums(4)); // [ [1, 1, 1, 1], [1, 1, 2], [1, 3], [2, 2] ]
