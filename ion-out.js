016433cf43c23a7953e76460a6ac16fe1182814a
skisenge <skisengese@gmail.com>
2024-08-30 17:18:21 +0300

    feat: initial commit

---
+++ ion-out.js
@@ -0,0 +1,9 @@
+function ionOut(inputStr) {
+    const regex = /\b\w*tion\b/g;
+    const matches = inputStr.match(regex) || [];
+
+    return matches.map(match => match.replace('ion', ''));
+}
+
+console.log(ionOut("The nation is in motion with a notion of action.")); // ["nat", "mot", "not", "act"]
+console.log(ionOut("This funtcion should return an array.")); // []
