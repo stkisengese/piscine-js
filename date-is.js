de8320477991b93530d42a2a0e4f56dcae112d38
skisenge <skisengese@gmail.com>
2024-09-02 19:43:19 +0300

    fix: time stamp conversion 2

--- date-is.js
+++ date-is.js
@@ -7,11 +7,23 @@ function isValid(date) {
 
 //checks if the fist date is after the second date
 function isAfter(date1, date2) {
+    if (typeof date1 === 'number') {
+        date1 = new Date(date1);
+    }
+    if (typeof date2 === 'number') {
+        date2 = new Date(date2);
+    }
     return isValid(date1) && isValid(date2) && date1.getTime() > date2.getTime();
 }
 
 // checks if first date is before the second date
 function isBefore(date1, date2) {
+    if (typeof date1 === 'number') {
+        date1 = new Date(date1);
+    }
+    if (typeof date2 === 'number') {
+        date2 = new Date(date2);
+    }
     return isValid(date1) && isValid(date2) && date1.getTime() < date2.getTime();
 }
 
@@ -32,6 +44,7 @@ console.log(isValid(date1));
 console.log(isValid(Date.now())); 
 console.log(isValid(new Date('1995-12-17T03:24:00').getTime())); 
 console.log(isAfter(date1, date2)); 
+console.log(isAfter(123123, 526));
 console.log(isBefore(date1, date2));
 console.log(isFuture(date1)); 
 console.log(isPast(date2)); 
