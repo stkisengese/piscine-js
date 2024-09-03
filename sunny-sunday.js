06269b415d82547aaabba013689b0768452cf354
skisenge <skisengese@gmail.com>
2024-09-03 19:43:18 +0300

    fix undefined function

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,8 +1,14 @@
 const weekdayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function addWeek(date) {
+function sunnySunday(date) {
     const epoch = new Date('0001-01-01');
     const dayDifference = Math.floor((date - epoch) / (24*60*60*1000));
     const dayIndex = dayDifference % 6;
     return weekdayNames[dayIndex];
 }
+
+console.log(sunnySunday(new Date('0001-01-01'))); // Output: Monday
+console.log(sunnySunday(new Date('0001-01-02'))); // Output: Tuesday
+console.log(sunnySunday(new Date('0001-01-07'))); // Output: Monday
+console.log(sunnySunday(new Date('0001-01-08'))); // Output: Tuesday
+console.log(sunnySunday(new Date('0001-01-09'))); // Output: Wednesday
