cc5b602370c88e82be64edb8256d762c4cc92c23
skisenge <skisengese@gmail.com>
2024-09-04 13:52:54 +0300

    fix: missing fields on trimmedTemp()

--- using-map.js
+++ using-map.js
@@ -16,8 +16,8 @@ const fahrenheitToCelsius = (arrFahrenheit) =>
     );
 
 const trimTemp = (arrObj) => 
-    arrObj.map(({ city, temperature }) => 
-        ({ city, temperature: temperature.replace(/\s+/g, '') })
+    arrObj.map( item  => 
+        ({ ...item, temperature: item.temperature.replace(/\s+/g, '') })
     );
 
 const tempForecasts = (arrObj) =>
@@ -26,32 +26,74 @@ const tempForecasts = (arrObj) =>
     );
 
 
-
-console.log(citiesOnly([
+console.log(trimTemp([
     {
       city: 'Los Angeles',
-      temperature: '  101 °F   ',
+      state: 'california',
+      region: 'West',
+      temperature: '101°F',
     },
     {
       city: 'San Francisco',
-      temperature: ' 84 ° F   ',
+      state: 'california',
+      region: 'West',
+      temperature: '84°F',
     },
-  ])) // -> ['Los Angeles', 'San Francisco']
+    { city: 'Miami', state: 'Florida', region: 'South', temperature: '112°F' },
+    {
+      city: 'New York City',
+      state: 'new york',
+      region: 'North East',
+      temperature: '0°F',
+    },
+    { city: 'Juneau', state: 'Alaska', region: 'West', temperature: '21°F' },
+    {
+      city: 'Boston',
+      state: 'massachussetts',
+      region: 'North East',
+      temperature: '45°F',
+    },
+    {
+      city: 'Jackson',
+      state: 'mississippi',
+      region: 'South',
+      temperature: '70°F',
+    },
+    { city: 'Utqiagvik', state: 'Alaska', region: 'West', temperature: '-1°F' },
+    {
+      city: 'Albuquerque',
+      state: 'new mexico',
+      region: 'West',
+      temperature: '95°F',
+    },
+  ])
+);
 
-console.log(upperCasingStates(['alabama', 'new jersey'])) // -> ['Alabama', 'New Jersey']
-console.log(fahrenheitToCelsius(['68°F', '59°F', '25°F'])) // -> ['20°C', '15°C', '-4°C']
-console.log(trimTemp([
-    { city: 'Los Angeles', temperature: '  101 °F   ' },
-    { city: 'San Francisco', temperature: ' 84 ° F   ' },
-  ])) /* -> [
-    { city: 'Los Angeles', temperature: '101°F' },
-    { city: 'San Francisco', temperature: '84°F' },
-  ] */
-    console.log(tempForecasts([
-        {
-          city: 'Pasadena',
-          temperature: ' 101 ° F',
-          state: 'california',
-          region: 'West',
-        },
-      ])); // -> ['38°C in Pasadena, California']
+// console.log(citiesOnly([
+//     {
+//       city: 'Los Angeles',
+//       temperature: '  101 °F   ',
+//     },
+//     {
+//       city: 'San Francisco',
+//       temperature: ' 84 ° F   ',
+//     },
+//   ])) // -> ['Los Angeles', 'San Francisco']
+
+// console.log(upperCasingStates(['alabama', 'new jersey'])) // -> ['Alabama', 'New Jersey']
+// console.log(fahrenheitToCelsius(['68°F', '59°F', '25°F'])) // -> ['20°C', '15°C', '-4°C']
+// console.log(trimTemp([
+//     { city: 'Los Angeles', temperature: '  101 °F   ' },
+//     { city: 'San Francisco', temperature: ' 84 ° F   ' },
+//   ])) /* -> [
+//     { city: 'Los Angeles', temperature: '101°F' },
+//     { city: 'San Francisco', temperature: '84°F' },
+//   ] */
+//     console.log(tempForecasts([
+//         {
+//           city: 'Pasadena',
+//           temperature: ' 101 ° F',
+//           state: 'california',
+//           region: 'West',
+//         },
+//       ])); // -> ['38°C in Pasadena, California']
