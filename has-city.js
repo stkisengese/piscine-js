d575e9cabf0546e6d4beea217c7442bff42d5849
skisenge <skisengese@gmail.com>
2024-09-05 20:13:31 +0300

    style: code refactoring

--- has-city.js
+++ has-city.js
@@ -1,9 +1,13 @@
-function hasCity(country, cities) {
-    return function(city) {
-      if (cities.includes(city)) {
-        return `${city} is a city from ${country}`;
-      } else {
-        return `${city} is not a city from ${country}`;
-      }
-    };
+// function hasCity(country, cities) {
+//     return function(city) {
+//       if (cities.includes(city)) {
+//         return `${city} is a city from ${country}`;
+//       } else {
+//         return `${city} is not a city from ${country}`;
+//       }
+//     };
+// }
+
+const hasCity = (country, cities) => {
+  return city => cities.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`;
 }
