0469a1cbc83ee8d7ed21ada3b9b1499e97e85fff
skisenge <skisengese@gmail.com>
2024-09-05 19:05:05 +0300

    fix: declared example

--- currify.js
+++ currify.js
@@ -10,10 +10,10 @@ function currify(fn) {
   };
 }
 
-const mult2 = (el1, el2) => el1 * el2;
-console.log(mult2(2, 2)); // result expected 4
+// const mult2 = (el1, el2) => el1 * el2;
+// console.log(mult2(2, 2)); // result expected 4
 
-const mult2Curried = currify(mult2);
+// const mult2Curried = currify(mult2);
 
-console.log(mult2Curried(2)(2)); // result expected 4
-// (same result, with a function that has technically only one argument)
+// console.log(mult2Curried(2)(2)); // result expected 4
+// // (same result, with a function that has technically only one argument)
