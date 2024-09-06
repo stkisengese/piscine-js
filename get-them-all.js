d8ad4364f73fea099858d4204334b7f97486c033
skisenge <skisengese@gmail.com>
2024-09-06 14:22:57 +0300

    refactor: removed partition function

--- get-them-all.js
+++ get-them-all.js
@@ -1,34 +1,40 @@
-const partition = (array, callback) => [
-    array.filter(callback),
-    array.filter((...args) => !callback(...args))
-];
-
 // Function to get architects and non-architects
 export const getArchitects = () => {
-    const architects = Array.from(document.getElementsByTagName('a'));
-    const nonArchitects = Array.from(document.getElementsByTag('span'));
-    return [architects, nonArchitects];
+  const architects = Array.from(document.getElementsByTagName("a"));
+  const nonArchitects = Array.from(document.getElementsByTagName("span"));
+  return [architects, nonArchitects];
 };
-  
+
 // Function to get classical and non-classical architects
 export const getClassical = () => {
-    const [architects] = getArchitects();
-    return partition(architects, architect => architect.classList.contains('classical'));
+  const architects = getArchitects()[0];
+  const classical = architects.filter((architect) =>
+    architect.classList.contains("classical")
+  );
+  const nonClassical = architects.filter(
+    (architect) => !architect.classList.contains("classical")
+  );
+  return [classical, nonClassical];
 };
 
-  
 // Function to get active and non-active classical architects
 export const getActive = () => {
-    const [classicalArchitects] = getClassical();
-    return partition(classicalArchitects, architect => architect.classList.contains('active'));
+  const classical = getClassical()[0];
+  const active = classical.filter((architect) =>
+    architect.classList.contains("active")
+  );
+  const nonActive = classical.filter(
+    (architect) => !architect.classList.contains("active")
+  );
+  return [active, nonActive];
 };
 
-  
 // Function to get Bonanno Pisano and other active classical architects
 export const getBonannoPisano = () => {
-    const [activeClassicalArchitects] = getActive();
-    const bonannoPisano = activeClassicalArchitects.find(architect => architect.id === 'BonannoPisano');
-    const remainingArchitects = activeClassicalArchitects.filter(architect => architect.id !== 'BonannoPisano');
-
-    return [bonannoPisano, remainingArchitects];
+  const active = getActive()[0];
+  const bonannoPisano = document.getElementById("BonannoPisano");
+  const otherActiveClassical = active.filter(
+    (architect) => architect.id !== "BonannoPisano"
+  );
+  return [bonannoPisano, otherActiveClassical];
 };
