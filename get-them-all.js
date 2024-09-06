5f621987261822102887d70d67c04d8141a14307
skisenge <skisengese@gmail.com>
2024-09-06 13:57:56 +0300

    feat: initial commit

---
+++ get-them-all.js
@@ -0,0 +1,33 @@
+const partition = (array, callback) => [
+    array.filter(callback),
+    array.filter((...args) => !callback(...args))
+];
+
+// Function to get architects and non-architects
+export const getArchitects = () => {
+    const allPeople = Array.from(document.getElementsByTagName('a'));
+    return partition(allPeople, person => person.tagName.toLowerCase() === 'a');
+};
+  
+// Function to get classical and non-classical architects
+export const getClassical = () => {
+    const [architects] = getArchitects();
+    return partition(architects, architect => architect.classList.contains('classical'));
+};
+
+  
+// Function to get active and non-active classical architects
+export const getActive = () => {
+    const [classicalArchitects] = getClassical();
+    return partition(classicalArchitects, architect => architect.classList.contains('active'));
+};
+
+  
+// Function to get Bonanno Pisano and other active classical architects
+export const getBonannoPisano = () => {
+    const [activeClassicalArchitects] = getActive();
+    const bonannoPisano = activeClassicalArchitects.find(architect => architect.id === 'BonannoPisano');
+    const remainingArchitects = activeClassicalArchitects.filter(architect => architect.id !== 'BonannoPisano');
+
+    return [bonannoPisano, remainingArchitects];
+};
--- has-city.js
+++ has-city.js
@@ -8,6 +8,17 @@
 //     };
 // }
 
-const hasCity = (country, cities) => {
-  return city => cities.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`;
-}
+// const hasCity = (country, cities) => {
+//   return city => cities.includes(city) ? `${city} is a city from ${country}` : `${city} is not a city from ${country}`;
+// }
+
+const hasCity = (country, cities) =>
+  city => 
+    `${city} ${
+      cities.includes(city) ? 'is' : 'is not'
+    } a city from ${country}`;
+
+// Example usage:
+const citiesInKenya = hasCity('Kenya', ['Nairobi', 'Mombasa', 'Kisumu']);
+console.log(citiesInKenya('Nairobi')); 
+console.log(citiesInKenya('Dar es Salaam')); 
