b7fa7d16c9336dd4dcd22dc5d1b743177dbac377
skisenge <skisengese@gmail.com>
2024-09-11 00:01:21 +0300

    feat: initial commit for replica function

---
+++ replica.js
@@ -0,0 +1,38 @@
+function isObject(obj) {
+  return obj && typeof obj === "object" && !Array.isArray(obj);
+}
+
+function replica(target, ...sources) {
+  sources.forEach((source) => {
+    if (isObject(source)) {
+      Object.keys(source).forEach((key) => {
+        if (isObject(source[key])) {
+          if (!target[key]) {
+            Object.assign(target, { [key]: {} });
+          }
+          replica(target[key], source[key]);
+        } else {
+          Object.assign(target, { [key]: source[key] });
+        }
+      });
+    }
+  });
+  return target;
+}
+
+// Example usage:
+const target = { a: 1, b: { c: 2 } };
+const source1 = { b: { d: 3 } };
+const source2 = { e: 4 };
+
+const result = replica(target, source1, source2);
+console.log(result); // { a: 1, b: { c: 2, d: 3 }, e: 4 }
+
+const targetA = { a: 1, b: { c: 2 } };
+const sourceA1 = { b: { d: 3 }, e: 4 };
+const sourceA2 = { f: 5, g: { h: 6 } };
+
+const resultA = replica(targetA, sourceA1, sourceA2);
+
+console.log(resultA);
+// Expected output: { a: 1, b: { c: 2, d: 3 }, e: 4, f: 5, g: { h: 6 } }
