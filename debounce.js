f99401eae3770ccd7909a05b83dbaf0ac89ccc78
skisenge <skisengese@gmail.com>
2024-09-12 02:17:15 +0300

    fix: concurrent calls

--- debounce.js
+++ debounce.js
@@ -16,22 +16,23 @@ function debounce(func, wait) {
 // Debounce function with 'leading' option
 function opDebounce(func, wait, options = {}) {
   let timeout;
-  let isLeadingCall = true;
+  let lastCallTime = 0;
 
   return function executedFunction(...args) {
-    const later = () => {
-      timeout = null;
-      if (!options.leading) func(...args);
-    };
+    const currentTime = Date.now();
+    const isCooldownPeriodOver = currentTime - lastCallTime > wait;
 
-    const callNow = options.leading && isLeadingCall;
-
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-
-    if (callNow) {
-      isLeadingCall = false;
+    if (options.leading && isCooldownPeriodOver) {
+      lastCallTime = currentTime;
       func(...args);
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        lastCallTime = Date.now();
+        if (!options.leading || isCooldownPeriodOver) {
+          func(...args);
+        }
+      }, wait);
     }
   };
 }
