906afcce7ef9d3cc94f4211942ecbbad3239c1e4
skisenge <skisengese@gmail.com>
2024-09-12 03:48:21 +0300

    fix: function calls

--- throttle.js
+++ throttle.js
@@ -24,31 +24,49 @@ function throttle(func, wait) {
 }
 
 // Throttle function with 'trailing' and 'leading' options
-function opThrottle(fn, wait, options = {}) {
-  let timeout = null;
-  let lastCall = 0;
-  const { leading = true, trailing = true } = options;
+function opThrottle(func, wait, options = { leading: true, trailing: true }) {
+  let timeout,
+    lastCall = 0,
+    lastArgs,
+    lastThis;
+
+  const invokeFunc = (time) => {
+    lastCall = time;
+    timeout = null;
+    func.apply(lastThis, lastArgs);
+    lastThis = lastArgs = null;
+  };
+
+  const leadingEdge = (time) => {
+    lastCall = time;
+    if (options.leading) {
+      invokeFunc(time);
+    }
+  };
+
+  const trailingEdge = () => {
+    if (options.trailing && lastArgs) {
+      invokeFunc(Date.now());
+    }
+    timeout = null;
+  };
 
   return function (...args) {
     const now = Date.now();
-
-    if (!lastCall && !leading) lastCall = now;
-
     const remaining = wait - (now - lastCall);
+    lastThis = this;
+    lastArgs = args;
 
-    if (remaining <= 0) {
+    if (remaining <= 0 || remaining > wait) {
       if (timeout) {
         clearTimeout(timeout);
         timeout = null;
       }
       lastCall = now;
-      fn.apply(this, args);
-    } else if (trailing && !timeout) {
-      timeout = setTimeout(() => {
-        lastCall = leading ? 0 : Date.now();
-        timeout = null;
-        fn.apply(this, args);
-      }, remaining);
+      func.apply(lastThis, lastArgs);
+      lastThis = lastArgs = null;
+    } else if (!timeout && options.trailing) {
+      timeout = setTimeout(trailingEdge, remaining);
     }
   };
 }
