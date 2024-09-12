d4cc3e41d91715e90d44274cc9eea2449fdaeb7f
skisenge <skisengese@gmail.com>
2024-09-13 01:10:24 +0300

    fix: track error caught format

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -12,7 +12,7 @@ function retry(count, callback) {
         attempts++;
 
         if (attempts > count) {
-          throw new Error(`${attempts}:${lastError}`);
+          throw new Error(`x:${args[0]}`);
         }
       }
     }
