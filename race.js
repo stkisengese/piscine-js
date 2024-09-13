5a0f320d47960d29db28f2e7b36cd7e60016b0d0
skisenge <skisengese@gmail.com>
2024-09-14 01:50:57 +0300

    fix: fix no. of promises tracking

--- race.js
+++ race.js
@@ -11,35 +11,28 @@ function race(promises) {
 }
 
 function some(promises, count) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (promises.length === 0 || count === 0) {
       resolve([]);
       return;
     }
 
-    const results = [];
+    const results = new Array(promises.length).fill(undefined);
     let resolvedCount = 0;
-    let rejectedCount = 0;
 
     promises.forEach((promise, index) => {
       Promise.resolve(promise)
         .then((value) => {
-          if (resolvedCount < count) {
-            results[index] = value;
-            resolvedCount++;
+          results[index] = value;
+          resolvedCount++;
 
-            if (resolvedCount === count) {
-              resolve(results.filter((result) => result !== undefined));
-            }
+          if (resolvedCount === count) {
+            resolve(results.slice(0, promises.length));
           }
         })
         .catch(() => {
-          rejectedCount++;
-          if (
-            rejectedCount + resolvedCount === promises.length &&
-            resolvedCount < count
-          ) {
-            resolve(results.filter((result) => result !== undefined));
+          if (resolvedCount === count) {
+            resolve(results.slice(0, promises.length));
           }
         });
     });
