4b8963d54d3ca27391b6e3cbc3072a4553381ac2
skisenge <skisengese@gmail.com>
2024-09-13 23:38:15 +0300

    feat: initial commit for custom promise.All

---
+++ all.js
@@ -0,0 +1,25 @@
+function all(obj) {
+  return new Promise((resolve, reject) => {
+    const keys = Object.keys(obj);
+    const result = {};
+    let completed = 0;
+
+    if (keys.length === 0) {
+      resolve(result);
+      return;
+    }
+
+    keys.forEach((key) => {
+      Promise.resolve(obj[key])
+        .then((value) => {
+          result[key] = value;
+          completed++;
+
+          if (completed === keys.length) {
+            resolve(result);
+          }
+        })
+        .catch(reject);
+    });
+  });
+}
